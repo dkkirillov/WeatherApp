8a43be725da2380d8626500eb70c937d8e1a8905
Dimitri Kirillov <dimique@Dimitris-iMac.broadband>
2021-01-30 20:04:16 +0000

    First commit

---
+++ WeatherApp/Model/WeatherData.swift
@@ -0,0 +1,23 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by Dimitri Kirillov on 06/11/2020.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherData: Codable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Codable {
+    let temp: Double
+}
+
+struct Weather: Codable {
+    let id: Int
+}
